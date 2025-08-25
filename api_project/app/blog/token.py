059a9a5c53181336fb4427666c58978db3941d23
Siddartha57 <112617980+Siddartha57@users.noreply.github.com>
2025-08-25 10:45:42 +0530

    token.py

--- api_project/app/blog/token.py
+++ api_project/app/blog/token.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 import jwt
 from jwt.exceptions import InvalidTokenError
-from blog import schemas
+from api_project.blog import schemas
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
@@ -25,4 +25,5 @@ def verify_token(token,credentials_exception):
             raise credentials_exception
         token_data = schemas.TokenData(email=email)
     except InvalidTokenError:
+
         raise credentials_exception
