0fb8e6dfdd7d1c7069654b44099eb11d2dbbcb2d
Siddartha57 <112617980+Siddartha57@users.noreply.github.com>
2025-08-25 10:53:25 +0530

    oauth2.py

--- api_project/app/blog/oauth2.py
+++ api_project/app/blog/oauth2.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer
 from fastapi import HTTPException, Depends,status
-from api_project.blog import token
+from api_project.app.blog import token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -15,3 +15,4 @@ def get_current_user(data: str = Depends(oauth2_scheme)):
 
 
     return token.verify_token(data,credentials_exception)
+
